12dc73ebd16f6c51c52868859b1f6f5a0716a9ca
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-11-11 08:12:29 +0300

    layout

--- corebits/src/app/layout.tsx
+++ corebits/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0F172A] text-[#F1F5F9]`}
       >
   {/* Particle background is a client-side component positioned behind all content */}
